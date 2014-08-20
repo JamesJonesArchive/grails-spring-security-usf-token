9fdb78b13035b4f9e4461d6a94a61b0eef75857d
James Jones <jamjon3@gmail.com>
2014-08-20 10:45:41 -0400

    Turned off transactions so this can be used as a reference in a bean without a transactionManager

--- grails-app/services/edu/usf/cims/token/UsfTokenService.groovy
+++ grails-app/services/edu/usf/cims/token/UsfTokenService.groovy
@@ -3,7 +3,7 @@ package edu.usf.cims.token
 import grails.converters.*
 
 class UsfTokenService {
-
+    static transactional = false
     def validate(String validateUrl,String webappId,String jwtoken) {
         return withRest(uri: validateUrl) {
             return post(body : [webappId:webappId,token:jwtoken],requestContentType : 'application/x-www-form-urlencoded') { resp, reader ->
