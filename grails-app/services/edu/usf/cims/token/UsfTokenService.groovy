9f0a5c1b665d8c5d5ccb42028b2d0c022737965a
James Jones <jamjon3@gmail.com>
2014-08-20 16:05:24 -0400

    Added methods similar to Eric's CAS implementation

--- grails-app/services/edu/usf/cims/token/UsfTokenService.groovy
+++ grails-app/services/edu/usf/cims/token/UsfTokenService.groovy
@@ -1,9 +1,13 @@
 package edu.usf.cims.token
 
 import grails.converters.*
+import grails.plugins.springsecurity.SpringSecurityService
+import org.springframework.web.context.request.RequestContextHolder
 
-class UsfTokenService {
+class UsfTokenService extends SpringSecurityService {
     static transactional = false
+    static scope = "singleton"
+    
     def validate(String validateUrl,String webappId,String jwtoken) {
         return withRest(uri: validateUrl) {
             return post(body : [webappId:webappId,token:jwtoken],requestContentType : 'application/x-www-form-urlencoded') { resp, reader ->
@@ -15,5 +19,18 @@ class UsfTokenService {
                 }                
             }
         }
+    }
+    
+    def getUsername(){
+        super.authentication.name
+    }
+    def getAttributes(){
+        RequestContextHolder.requestAttributes.request.userPrincipal.assertion.principal.attributes
+    }
+    def getEppa() {
+        RequestContextHolder.requestAttributes.request.userPrincipal.assertion.principal.attributes.eduPersonPrimaryAffiliation
+    }
+    def getAffiliation(){
+        RequestContextHolder.requestAttributes.request.userPrincipal.assertion.principal.attributes.eduPersonAffiliation
     }
 }
