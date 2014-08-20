5f3bf565b0fc5c51457f27bc1048075b635d07d7
James Jones <jamjon3@gmail.com>
2014-08-20 14:09:11 -0400

    All the Angular SPA initial commit

---
+++ test/projects/UsfTokenPluginTest/web-app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name usfTokenPluginTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the usfTokenPluginTestApp
+ */
+angular.module('usfTokenPluginTestApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ test/projects/UsfTokenPluginTest/web-app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name usfTokenPluginTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the usfTokenPluginTestApp
+ */
+angular.module('usfTokenPluginTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
