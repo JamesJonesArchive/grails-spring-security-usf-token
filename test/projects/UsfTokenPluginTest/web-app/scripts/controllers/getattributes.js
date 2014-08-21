fc9df38b2575cd10b8d6f2964b992fad08b94fa1
James Jones <jamjon3@gmail.com>
2014-08-21 13:09:37 -0400

    Added the other controllers for the test app

---
+++ test/projects/UsfTokenPluginTest/web-app/scripts/controllers/getattributes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name usfTokenPluginTestApp.controller:GetattributesCtrl
+ * @description
+ * # GetattributesCtrl
+ * Controller of the usfTokenPluginTestApp
+ */
+angular.module('usfTokenPluginTestApp')
+  .controller('GetattributesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ test/projects/UsfTokenPluginTest/web-app/scripts/controllers/geteppa.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name usfTokenPluginTestApp.controller:GeteppaCtrl
+ * @description
+ * # GeteppaCtrl
+ * Controller of the usfTokenPluginTestApp
+ */
+angular.module('usfTokenPluginTestApp')
+  .controller('GeteppaCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
