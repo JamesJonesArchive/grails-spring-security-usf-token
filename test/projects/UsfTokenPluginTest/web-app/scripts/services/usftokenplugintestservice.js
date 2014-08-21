6b2f85c11583d5445ca3b9e8d7a62a16c1c0032e
James Jones <jamjon3@gmail.com>
2014-08-21 13:42:45 -0400

    Finished the other service methods

--- test/projects/UsfTokenPluginTest/web-app/scripts/services/usftokenplugintestservice.js
+++ test/projects/UsfTokenPluginTest/web-app/scripts/services/usftokenplugintestservice.js
@@ -1,26 +1,46 @@
-'use strict';
-
-/**
- * @ngdoc service
- * @name usfTokenPluginTestApp.UsfTokenPluginTestService
- * @description
- * # UsfTokenPluginTestService
- * Factory in the usfTokenPluginTestApp.
- */
-angular.module('usfTokenPluginTestApp')
-  .factory('UsfTokenPluginTestService',['$resource','tokenAuth', function ($resource,tokenAuth) {
-    
-    // Service logic
-    var tokenTestResource = $resource(tokenAuth.getResourceUrl('tokenTest'),{},{
-        'getUsername': {
-          method: 'GET', params: {},responseType: 'json', headers: { 'X-Auth-Token': tokenAuth.getStoredToken('tokenTest') }
+(function (window, angular, undefined) {
+  'use strict';
+  
+  /**
+   * @ngdoc service
+   * @name usfTokenPluginTestApp.UsfTokenPluginTestService
+   * @description
+   * # UsfTokenPluginTestService
+   * Factory in the usfTokenPluginTestApp.
+   */
+  angular.module('usfTokenPluginTestApp')
+    .factory('UsfTokenPluginTestService',['$resource','tokenAuth', function ($resource,tokenAuth) {
+      
+      // Service logic
+      var tokenResources = {
+        getUsernameResource: $resource(tokenAuth.getResourceUrl('getUsername'),{},{
+            'getUsername': {
+              method: 'GET', params: {},responseType: 'json', headers: { 'X-Auth-Token': tokenAuth.getStoredToken('getUsername') }
+            }
+        }),
+        getAttributesResource: $resource(tokenAuth.getResourceUrl('getAttributes'),{},{
+            'getAttributes': {
+              method: 'GET', params: {},responseType: 'json', headers: { 'X-Auth-Token': tokenAuth.getStoredToken('getAttributes') }
+            }
+        }),
+        getEppaResource: $resource(tokenAuth.getResourceUrl('getEppa'),{},{
+            'getEppa': {
+              method: 'GET', params: {},responseType: 'json', headers: { 'X-Auth-Token': tokenAuth.getStoredToken('getEppa') }
+            }
+        })
+      };
+      
+      // Public API here
+      return {
+        getUsername: function () {
+          return tokenResources.getUsernameResource.getUsername().$promise;
+        },
+        getAttributes: function () {
+          return tokenResources.getAttributesResource.getAttributes().$promise;
+        },
+        getEppa: function () {
+          return tokenResources.getEppaResource.getEppa().$promise;
         }
-    });
-    
-    // Public API here
-    return {
-      getUsername: function () {
-        return tokenTestResource.getUsername().$promise;
-      }
-    };
-  }]);
+      };
+    }]);
+})(window, window.angular);
