e4ee01a924755f6d1b3571e96a6c0fda1be7ba63
James Jones <jamjon3@gmail.com>
2014-09-11 11:11:01 -0400

    Almost a complete rewrite. Handles authentication by token with a custom authentication token

--- src/groovy/edu/usf/cims/token/UsfTokenAuthenticationFilter.groovy
+++ src/groovy/edu/usf/cims/token/UsfTokenAuthenticationFilter.groovy
@@ -9,6 +9,7 @@ package edu.usf.cims.token
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.web.util.NestedServletException
+import java.text.MessageFormat
 import javax.servlet.FilterChain
 import javax.servlet.ServletException
 import javax.servlet.ServletRequest
@@ -22,7 +23,8 @@ import org.springframework.security.authentication.AuthenticationServiceExceptio
 import org.springframework.security.authentication.LockedException
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.AuthenticationException
-// import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.core.context.SecurityContextHolder
+import edu.usf.cims.token.UsfTokenAuthenticationException
 /**
  *
  * @author james
@@ -30,47 +32,72 @@ import org.springframework.security.core.AuthenticationException
 class UsfTokenAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
     private static final logger = LogFactory.getLog(this)
     def tokenHeader
+    def authenticationSuccessHandler
+    def authenticationFailureHandler
     public UsfTokenAuthenticationFilter(String defaultFilterProcessesUrl) {
         super(defaultFilterProcessesUrl);
-        // super.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher(defaultFilterProcessesUrl));
         setAuthenticationManager(new UsfTokenAuthenticationManager());
-        // setAuthenticationSuccessHandler(new AuthenticationSuccessHandler());
     }    
     /**
      * Attempt to authenticate request - basically just pass over to another method to authenticate request headers
      */
     @Override public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        String token = request.getHeader(tokenHeader);
-        logger.info("token found:"+token);
-        AbstractAuthenticationToken userAuthenticationToken = authUserByToken(token);
-        if(userAuthenticationToken == null) throw new AuthenticationServiceException(MessageFormat.format("Error | {0}", "Bad Token"));
-        return userAuthenticationToken;
-    }
-    /**
-     * authenticate the user based on token
-     * @return
-     */
-    private AbstractAuthenticationToken authUserByToken(String token) {
+        String token = request.getHeader(tokenHeader)
+        logger.info("token found:"+token)
         if(token==null) {
             return null;
         }
-        AbstractAuthenticationToken authToken = new UsfAuthenticationToken(token);
+        UsfAuthenticationToken userAuthenticationToken
         try {
-            return authToken;
+            userAuthenticationToken = new UsfAuthenticationToken(token)
         } catch (Exception e) {
-            logger.error("Authenticate user by token error: ", e);
+            logger.error("Authenticate user by token error: " + e.getMessage());
+            println "Authenticate user by token error: " + e.getMessage();
+            throw new UsfTokenAuthenticationException(e.getMessage())
         }
-        return authToken;
+        if(userAuthenticationToken == null) throw new AuthenticationServiceException(MessageFormat.format("Error | {0}", "Bad Token"));
+        return this.getAuthenticationManager().authenticate(userAuthenticationToken)
     }
+    
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,Authentication authResult) throws IOException, ServletException {
-        SecurityContextHolder.getContext().setAuthentication(authResult);
-
-        getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
+        SecurityContextHolder.getContext().setAuthentication(authResult)
     }
     @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request,HttpServletResponse response,AuthenticationException failed) {
+        SecurityContextHolder.clearContext()
+        logger.debug('JWT Token-based authentication failed: ' + failed.toString())
+        authenticationFailureHandler.onAuthenticationFailure(request,response,failed)  
+    }
     public void doFilter(ServletRequest req, ServletResponse res,FilterChain chain) throws IOException, ServletException {
-        super.doFilter(req, res, chain);
+        println "Is this filter running?"
+        HttpServletRequest request = (HttpServletRequest) req
+        HttpServletResponse response = (HttpServletResponse) res
+        String token = request.getHeader(tokenHeader)
+        /**
+         * If token doesn't exist OR is invalid, send back to filters
+         **/
+        if(!token) {
+            chain.doFilter(request, response)
+            return                    
+        }
+            
+        logger.debug('Request requires jwt-token-based authentication')
+
+        Authentication authentication
+
+        try {
+            authentication = attemptAuthentication(request,response)
+            if(!authentication) {
+                return
+            }
+            println "Successful Auth"            
+            successfulAuthentication(request, response, authentication)
+            chain.doFilter(request, response)
+        } catch(AuthenticationException failed) {
+            unsuccessfulAuthentication(request, response, failed)
+            return
+        }
     }
 }
 
